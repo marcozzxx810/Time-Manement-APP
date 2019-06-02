e87034ef067e87def3469d3083b0563352f84825
marco mak <marcowingwing@gmail.com>
2019-06-02 23:21:52 +0800

    Datebase info update function added

--- screens/DashboardScreen.js
+++ screens/DashboardScreen.js
@@ -2,14 +2,16 @@ import React, { Component } from "react";
 import { 
     View,
     Text,
-    StyleSheet
+    StyleSheet,
+    Button
 } from "react-native";
-
+import firebase from "firebase";
 class DashboardScreen extends Component {
     render() {
         return (
             <View style={styles.container}>
                 <Text>DashboardScreen</Text>
+                <Button title='Sign out' onPress={() => firebase.auth().signOut()} />
             </View>
         );
     }
